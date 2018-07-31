1cd983ba494e92ff07b005654e26f1c61bc05854
urielc137 <uriel.c@bronxsoftware.org>
2018-07-31 17:16:41 -0400

    New JS

--- Cssi-Labs/javaScript/labs/Alarm_Clock/script.js
+++ Cssi-Labs/javaScript/labs/Alarm_Clock/script.js
@@ -13,3 +13,28 @@
 // limitations under the License.
 
 console.log("script is running...");
+function Basic_Alarm(alarmMessage){
+  return alarmMessage;
+};
+console.log(Basic_Alarm("My alarm!"));
+
+function My_Alarm(time){ // time = 7:00
+  return "Hey Uriel, wake up! It's " + time;
+}
+console.log(My_Alarm("7:00 am"));
+
+function Mom_Alarm(time){
+  return "Hey Mom, wake up! It's " + time;
+}
+console.log(Mom_Alarm("8:00 am"));
+
+function Family_Alarm(time, name){
+  return "Hey " + name+ ", wake up! It's " + time;
+}
+console.log(Family_Alarm("9:00am", "Dave"));
+
+function Important_Alarm(message){
+  return message.toUpperCase();
+}
+
+console.log(Important_Alarm("Wake up plesde"));
--- Lab-JS1/app.js
+++ Lab-JS1/app.js
@@ -1,3 +1,14 @@
 function sendAlert() {
-  alert("Hello World");
+  console.log("Hello CSSI");
+  var name1 = "Bob";
+  let name2 ="Bill";
+  const name3 = "Bo";
+
+
+  name1 = "Amy";
+  name2 = "Alice";
+  name3 = "Ali";
+  console.log(name1);
+  console.log(name2);
+  console.log(name3);
 }
