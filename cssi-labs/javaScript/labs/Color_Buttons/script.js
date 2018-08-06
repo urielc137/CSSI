68db5da63b3832cfa5d1d07199f0c08765296004
urielc137 <uriel.c@bronxsoftware.org>
2018-08-06 06:52:53 -0400

    08/04/18

--- cssi-labs/javaScript/labs/Color_Buttons/script.js
+++ cssi-labs/javaScript/labs/Color_Buttons/script.js
@@ -13,9 +13,54 @@
 // limitations under the License.
 
 // Use querySelector to store the div in a variable.
+const colors=['red','green','blue', 'white'];
 let redButton = document.querySelector('#red');
+let greenButton = document.querySelector('#green');
+let blueButton = document.querySelector('#blue');
+let clearButton = document.querySelector('#clear');
+let box = document.querySelector('#responseBox');
 
 // Use addEventListener to respond to a click event.
-redButton.addEventListener('click', (e) => {
+redButton.addEventListener('click', (switchcolor0) => {
   console.log("You clicked the red button!");
 })
+
+greenButton.addEventListener('click', (switchcolor1) => {
+  console.log("You clicked the green button!");
+})
+
+blueButton.addEventListener('click', (switchcolor2) => {
+  console.log("You clicked the blue button!");
+})
+
+clearButton.addEventListener('click', (switchcolor3) => {
+  console.log("You clicked the clear button!");
+})
+
+redButton.addEventListener('click', switchcolor0);
+
+function switchcolor0 (){
+  redButton.style.backgroundColor=colors[0];
+  box.style.backgroundColor=colors[0];
+}
+
+greenButton.addEventListener('click', switchcolor1);
+
+function switchcolor1 (){
+  greenButton.style.backgroundColor=colors[1];
+  box.style.backgroundColor=colors[1];
+}
+
+blueButton.addEventListener('click', switchcolor2);
+
+function switchcolor2 (){
+  blueButton.style.backgroundColor=colors[2];
+  box.style.backgroundColor=colors[2];
+}
+
+clearButton.addEventListener('click', switchcolor3);
+
+function switchcolor3 (){
+  clearButton.style.backgroundColor=colors[3];
+  box.style.backgroundColor=colors[3];
+}
