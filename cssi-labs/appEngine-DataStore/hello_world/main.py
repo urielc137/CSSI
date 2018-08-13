f5d437e102f5b5ec75f1bdaea7ec595cea90a846
urielc137 <uriel.c@bronxsoftware.org>
2018-08-13 12:26:18 -0400

    GAE stuff

--- cssi-labs/appEngine-DataStore/hello_world/main.py
+++ cssi-labs/appEngine-DataStore/hello_world/main.py
@@ -13,14 +13,52 @@
 # limitations under the License.
 
 import webapp2
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
 
+        template_dic = {"country": "USA",
+                       "region_name": "North East",
+                       "region_num": 121,
+                       "url": "http://i.imgur.com/WavdxuN.png",
+                       "city": ["new york","boston", "philadelphia"],
+	                   "message": "welcome to: "
+                       }
+
+        self.response.write(welcome_template.render(template_dic))
+
+
+class getresults(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("hello from getresults")
+    def post(self):
+        results_template = the_jinja_env.get_template('templates/results.html')
+        firstname = self.request.get('firstname')
+        lastname = self.request.get('lastname')
+        age=self.request.get('age')
+
+        webform_dict = {"fn": firstname, "ln": lastname, "age":age}
+        self.response.write(results_template.render(webform_dict))
+        self.response.write(firstname)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/showresults', getresults),
 ], debug=True)
